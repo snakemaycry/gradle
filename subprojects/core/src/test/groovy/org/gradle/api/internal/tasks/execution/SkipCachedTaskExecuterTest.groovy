fce6d14c933978701d2826b7eda8e9ae5afb4a60
Luke Daley <luke@gradle.com>
2017-05-25 14:13:55 +1000

    Expose the “up to date messages” to build scans.

--- subprojects/core/src/test/groovy/org/gradle/api/internal/tasks/execution/SkipCachedTaskExecuterTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/api/internal/tasks/execution/SkipCachedTaskExecuterTest.groovy
@@ -88,7 +88,7 @@ class SkipCachedTaskExecuterTest extends Specification {
 
         then:
         1 * taskState.setOutcome(TaskExecutionOutcome.FROM_CACHE)
-        1 * taskState.setOriginBuildId(originId)
+        1 * taskContext.setOriginBuildId(originId)
         0 * _
     }
 
